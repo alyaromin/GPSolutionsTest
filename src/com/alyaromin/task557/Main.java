6e9fea152dee9409b96e131336928b1cdb5517f7
alyaromin <alyaromin@gmail.com>
2018-01-27 11:23:15 +0300

    changed input method

--- src/com/alyaromin/task557/Main.java
+++ src/com/alyaromin/task557/Main.java
@@ -1,48 +1,90 @@
 package com.alyaromin.task557;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	int m = sc.nextInt();
-	int n = sc.nextInt();
-	int a = sc.nextInt() - 1;
-	int b = sc.nextInt() - 1;
-	int p = sc.nextInt();
+    static StringTokenizer tokenizer;
+    static BufferedReader reader;
 
-	int[][] multipleMatrix = new int[n][n];
-	int[][] matrix1 = new int[n][n];
-	int[][] matrix2Transposed = new int[n][n];
+    public static void main(String[] args) throws Exception {
 
-	for (int x = 0; x < n; x++) {
-	    for (int y = 0; y < n; y++) {
-		multipleMatrix[x][y] = sc.nextInt();
-	    }
-	}
+	reader = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+
+	int m = nextInt();
+	int n = nextInt();
+	int a = nextInt() - 1;
+	int b = nextInt() - 1;
+	int p = nextInt();
+
+	List<int[][]> matrixes = new ArrayList<>();
+
+	matrixes.add(0, readMatrix(n));
+
 	for (int i = 1; i < m; i++) {
-	    for (int x = 0; x < n; x++) {
-		for (int y = 0; y < n; y++) {
-		    matrix1[x][y] = multipleMatrix[x][y];
-		    matrix2Transposed[y][x] = sc.nextInt();
-		    multipleMatrix[x][y] = 0;
-		}
-	    }
-	    for (int x = 0; x < n; x++) {
-		for (int y = 0; y < n; y++) {
-		    for (int k = 0; k < n; k++) {
-			multipleMatrix[x][y] += matrix1[x][k] * matrix2Transposed[y][k];
-			if (multipleMatrix[x][y] >= p) {
-			    multipleMatrix[x][y] = multipleMatrix[x][y] % p;
-			}
-		    }
-		}
+	    matrixes.add(i, readTransposedMatrix(n));
+	}
+
+	if (m >= 1) {
+	    for (int i = 1; i < m; i++) {
+		matrixes.set(0, multipleMatrixes(matrixes.get(0), matrixes.get(i), n, p));
 	    }
 	}
-	sc.close();
 
-	System.out.println(multipleMatrix[a][b]);
+	System.out.println(matrixes.get(0)[a][b]);
     }
 
+    private static int[][] readMatrix(final int columns) throws Exception {
+	int[][] matrix = new int[columns][columns];
+
+	for (int i = 0; i < columns; i++) {
+	    for (int j = 0; j < columns; j++) {
+		matrix[i][j] = nextInt();
+	    }
+	}
+	return matrix;
+    }
+
+    private static int[][] readTransposedMatrix(final int columns) throws Exception {
+	int[][] matrix = new int[columns][columns];
+
+	for (int i = 0; i < columns; i++) {
+	    for (int j = 0; j < columns; j++) {
+		matrix[j][i] = nextInt();
+	    }
+	}
+	return matrix;
+    }
+
+    private static int[][] multipleMatrixes(final int[][] matrix1, final int[][] transposedMatrix2, final int columns, final int module) {
+
+	int[][] resultMatrix = new int[columns][columns];
+
+	for (int i = 0; i < columns; i++) {
+	    for (int j = 0; j < columns; j++) {
+		int sum = 0;
+		for (int k = 0; k < columns; k++) {
+		    sum += matrix1[i][k] * transposedMatrix2[j][k];
+		}
+		resultMatrix[i][j] = sum % module;
+	    }
+	}
+	return resultMatrix;
+    }
+
+    private static int nextInt() throws Exception {
+	return Integer.parseInt(nextToken());
+    }
+
+    private static String nextToken() throws Exception {
+	while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+	    tokenizer = new StringTokenizer(reader.readLine());
+	}
+	return tokenizer.nextToken();
+    }
 }
