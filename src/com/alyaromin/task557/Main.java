241755e3697c6e0c084b78b12b6e2f09cd10ff4e
alyaromin <alyaromin@gmail.com>
2018-02-04 21:12:02 +0300

    changed names of variables

--- src/com/alyaromin/task557/Main.java
+++ src/com/alyaromin/task557/Main.java
@@ -16,59 +16,62 @@ public class Main {
 
 	reader = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
 
-	int m = nextInt();
-	int n = nextInt();
-	int a = nextInt() - 1;
-	int b = nextInt() - 1;
-	int p = nextInt();
+	final int countOfMatrixes = nextInt();
+	final int size = nextInt();
+	final int wantedRowIndex = nextInt() - 1;
+	final int wantedColumnIndex = nextInt() - 1;
+	final int module = nextInt();
 
 	List<int[][]> matrixes = new ArrayList<>();
 
-	matrixes.add(0, readMatrix(n));
+	matrixes.add(0, readMatrix(size, size));
 
-	for (int i = 1; i < m; i++) {
-	    matrixes.add(i, readTransposedMatrix(n));
+	for (int i = 1; i < countOfMatrixes; i++) {
+	    matrixes.add(i, readTransposedMatrix(size, size));
 	}
 
-	if (m >= 1) {
-	    for (int i = 1; i < m; i++) {
-		matrixes.set(0, multipleMatrixes(matrixes.get(0), matrixes.get(i), n, p));
+	if (countOfMatrixes >= 1) {
+	    for (int i = 1; i < countOfMatrixes; i++) {
+		matrixes.set(0, multiplyMatrixes(matrixes.get(0), matrixes.get(i), module));
 	    }
 	}
 
-	System.out.println(matrixes.get(0)[a][b]);
+	System.out.println(matrixes.get(0)[wantedRowIndex][wantedColumnIndex]);
     }
 
-    private static int[][] readMatrix(final int columns) throws Exception {
-	int[][] matrix = new int[columns][columns];
+    private static int[][] readMatrix(final int rowCount, final int columnCount) throws Exception {
+	int[][] matrix = new int[rowCount][columnCount];
 
-	for (int i = 0; i < columns; i++) {
-	    for (int j = 0; j < columns; j++) {
+	for (int i = 0; i < rowCount; i++) {
+	    for (int j = 0; j < columnCount; j++) {
 		matrix[i][j] = nextInt();
 	    }
 	}
 	return matrix;
     }
 
-    private static int[][] readTransposedMatrix(final int columns) throws Exception {
-	int[][] matrix = new int[columns][columns];
+    private static int[][] readTransposedMatrix(final int rowCount, final int columnCount) throws Exception {
+	int[][] matrix = new int[rowCount][columnCount];
 
-	for (int i = 0; i < columns; i++) {
-	    for (int j = 0; j < columns; j++) {
+	for (int i = 0; i < rowCount; i++) {
+	    for (int j = 0; j < columnCount; j++) {
 		matrix[j][i] = nextInt();
 	    }
 	}
 	return matrix;
     }
 
-    private static int[][] multipleMatrixes(final int[][] matrix1, final int[][] transposedMatrix2, final int columns, final int module) {
+    private static int[][] multiplyMatrixes(final int[][] matrix1, final int[][] transposedMatrix2, final int module) {
 
-	int[][] resultMatrix = new int[columns][columns];
+	final int rowCount = matrix1.length;
+	final int columnCount = rowCount;
 
-	for (int i = 0; i < columns; i++) {
-	    for (int j = 0; j < columns; j++) {
+	int[][] resultMatrix = new int[rowCount][columnCount];
+
+	for (int i = 0; i < rowCount; i++) {
+	    for (int j = 0; j < columnCount; j++) {
 		int sum = 0;
-		for (int k = 0; k < columns; k++) {
+		for (int k = 0; k < rowCount; k++) {
 		    sum += matrix1[i][k] * transposedMatrix2[j][k];
 		}
 		resultMatrix[i][j] = sum % module;
